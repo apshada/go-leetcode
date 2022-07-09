f28064c9a09f146f52461cd72e831baacbe2a599
Aditya Hada <apshada1@gmail.com>
2022-07-09 18:38:11 +0530

    updated problems

--- lc-1365/lc-1365.go
+++ lc-1365/lc-1365.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func main() {
 	nums := [5]int{8, 1, 2, 2, 3}
-	// fmt.Println(nums)
-	res := make(map[int]int, 0)
-
+	resData := make([]int, 0)
 	for i := 0; i < len(nums); i++ {
-		res[nums[i]]++
-		// for j := i + 1; j < len(nums); j++ {
-		// 	if nums[j] < nums[i] && j != i {
-		// 		res[nums[j]]++
-		// 	}
-		// }
+		c := 0
+		for j := 0; j < len(nums); j++ {
+			if nums[j] < nums[i] && j != i {
+				c++
+
+			}
+		}
+		resData = append(resData, c)
 	}
-	fmt.Println(res)
+	fmt.Println(resData)
 }
