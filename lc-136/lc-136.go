42b8a3772a9fc7431e9e52297b79ab8859705e5a
Aditya Hada <apshada1@gmail.com>
2022-07-21 23:02:09 +0530

    added lc-136

---
+++ lc-136/lc-136.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 2, 1}
+	res := make(map[int]int, 0)
+	count := 0
+	for _, num := range nums {
+		res[num]++
+	}
+
+	for key, val := range res {
+		if(val == 1){
+			count = key
+		}
+	}
+	fmt.Println(count)
+}
