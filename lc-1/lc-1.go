7ac9fc2f2042de491a10d3a9c6ba585b460f1361
Aditya Hada <apshada1@gmail.com>
2022-09-10 17:36:02 +0530

    added

--- lc-1/lc-1.go
+++ lc-1/lc-1.go
@@ -8,7 +8,7 @@ func main() {
 	res := make([]int, 0)
 	for i := 0; i < len(nums); i++ {
 		for j := 1; j < len(nums); j++ {
-			// fmt.Println(nums[i], nums[j])
+			fmt.Println(nums[i], nums[j])
 			if nums[i]+nums[j] == target {
 				fmt.Println(i, j)
 				res = append(res, i)
